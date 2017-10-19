9390f90368079226440dd5bc1fd2f6c08c5d7dce
汤阳 <tangyang@tangyangdeMac-mini.local>
2017-10-19 14:28:12 +0800

    super()

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.stateA = {
+      name: '访问者',
+      time: Date.now()
+    }
+    this.arr = ['item1', 'item2', 'item3']
+  }
+  clickAction () {
+    alert('clickAction')
+  }
+  message () {
+    return `${this.props.name}:${this.stateA.time}`
+  }
   render () {
     return (
       <div className='App'>
         <header className='App-header'>
-          <h1 className='App-title'>Welcome to React</h1>
+          <h1 className='App-title'>Welcome to</h1>
         </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <ul>
+          {this.arr.map(item => <li key={item}>{item}</li>)}
+        </ul>
+        <p>
+          {this.message()}
+        </p>
+        <p className='App-intro' onClick={this.clickAction}>
+          {this.props.name}
         </p>
       </div>
     )
